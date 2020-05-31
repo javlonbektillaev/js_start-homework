28069548a048058acea1df2b335e6c2e202879a2
javlon_tillaev <javlon_j_tillaev@mail.ru>
2020-05-31 09:53:02 +0300

    js_homework

---
+++ script/script.js
@@ -0,0 +1,39 @@
+var name= prompt('введите свое имя')
+name="вас зовут "+ name
+console.log(name);
+
+var age= prompt('введите ваш возраст')
+age="вам "+ age + " лет"
+console.log(age);
+
+var pr1= prompt('solve 12*5= ')
+var num1=12
+var num2=5
+var result1 = num1 * num2
+result1 = "The corect answer is " + result1 + " Your answer is " + pr1
+console.log(result1);
+
+var pr1= prompt('solve 6+3= ')
+var num3=6
+var num4=3
+var result2 = num3 + num4
+result2 = "The corect answer is " + result2 + " Your answer is " + pr1
+console.log(result2);
+
+var pr1= prompt('solve 20-5= ')
+var num5=20
+var num6=5
+var result3 = num5 - num6
+result3 = "The corect answer is " + result3 + " Your answer is " + pr1
+console.log(result3);
+
+var pr1= prompt('solve 10%8= ')
+var num7=10
+var num8=8
+var result4 = num7 % num8
+result4 = "The corect answer is " + result4 + " Your answer is " + pr1
+console.log(result4);
+alert("press the button console to see ur results")
+
+
+
